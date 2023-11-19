4949cd62d0a4f2e7c43b39ad50653ada14cc0a14
ricksterhd123 <ricksterhd123@gmail.com>
2023-11-19 00:57:06 +0000

    simplify unit test utility function

--- test/hello_test.ml
+++ test/hello_test.ml
@@ -1,10 +1,14 @@
 exception Error of string
 
-let expect value = if value then () else raise (Error "Test Failed");;
+let test test_name success =
+  if success then 
+    print_endline ("SUCCESS: " ^ test_name) 
+else 
+  raise (Error ("FAILED: " ^ test_name));;
 
-expect ((Hello.last [1]) = Some 1);;
-expect ((Hello.last []) = None);;
-expect ((Hello.last_two [1;2;3;4;5]) = Some (4, 5));;
-expect ((Hello.last_two [1]) = None);;
-expect ((Hello.last_two []) = None);;
-expect ((Hello.list_nth [1;2;3;4;5] 4) = Some 5);;
+test "#last.1" ((Hello.last [1]) = Some 1);;
+test "#last.2" ((Hello.last []) = None);;
+test "#last_two.1" ((Hello.last_two [1;2;3;4;5]) = Some (4, 5));;
+test "#last_two.2" ((Hello.last_two [1]) = None);;
+test "#last_two.3" ((Hello.last_two []) = None);;
+test "#last_nth.1" ((Hello.list_nth [1;2;3;4;5] 4) = Some 5);;
